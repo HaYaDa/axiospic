b78ce512e6e9ffcfbfc99b37d8c249127c6f80ff
H-Sun <hasanyasindagkus@hotmail.com>
2019-11-07 02:17:35 +0100

    Updates

---
+++ precache-manifest.c0a220297b5f8c023b446c68f85b797f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5f5f2e880ab768c7f5575590648f62",
+    "url": "/axiospic/index.html"
+  },
+  {
+    "revision": "ec59443e157552dd4386",
+    "url": "/axiospic/static/css/main.40d779a7.chunk.css"
+  },
+  {
+    "revision": "ae0cda2d0a4a41ef73f6",
+    "url": "/axiospic/static/js/2.409bc2c9.chunk.js"
+  },
+  {
+    "revision": "ec59443e157552dd4386",
+    "url": "/axiospic/static/js/main.a5a89916.chunk.js"
+  },
+  {
+    "revision": "481b287eae31c6f54d07",
+    "url": "/axiospic/static/js/runtime-main.a29f6d41.js"
+  }
+]);
